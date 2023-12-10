85078467da55e7b843471d433665c099e34c89cc
Gaurab Shrestha <gaurab.shrestha@mypay.com.np>
2023-12-10 15:34:25 +0545

    Commit

--- KushalBlogWebApp/Controllers/HomeController.cs
+++ KushalBlogWebApp/Controllers/HomeController.cs
@@ -17,24 +17,13 @@ namespace KushalBlogWebApp.Controllers
                 ViewBag.LatestPost = await _blogService.GetLatestPost();
                 ViewBag.AllBlogList = await _blogService.GetAllData();
                 ViewBag.AllPinnedBlogList = await _blogService.GetAllPinnedBlogList();
-
-
-            //    var blogList = await _blogService.GetAllData();
-            //    return View();
-            //var pageSize = 4;
-            //var currentPage = page ?? 1;
-
-            //var displayedBlogs = await _blogService.GetAllData(pageSize, currentPage);
-            //ViewBag.CurrentPage = currentPage;
-            //ViewBag.AllBlogList = displayedBlogs;
-            //ViewBag.TotalCount = displayedBlogs.Count();
-
             return View();
         }
         #region Single Blog Details
         [HttpGet]
         public async Task<IActionResult> SingleBlogDetails(int Id)
         {
+            ViewBag.AllPinnedBlogList = await _blogService.GetAllPinnedBlogList();
             var blogDetails = await _blogService.GetSingleBlogDetails(Id);
             return View(blogDetails);
          
